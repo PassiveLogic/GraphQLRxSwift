144029cee2d1517e2aa799ece80961c3bd21600f
Clack Cole <clack@passivelogic.com>
2024-12-18 17:21:03 -0700

    chore: Adopt a modified RxSwift to workaround an NSLock subclassing issue
    This workaround can be removed if/when the root cause is resolved in
    Foundation:
    https://github.com/swiftlang/swift-corelibs-foundation/issues/5108
    
    ...or when RxSwift has merged a workaround on its side:
    https://github.com/ReactiveX/RxSwift/issues/2621

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "2.0.0"),
         .package(url: "https://github.com/GraphQLSwift/Graphiti.git", from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.1.0"),
+        .package(url: "https://github.com/PassiveLogic/RxSwift.git", exact: "6.8.1-pl.1"),
     ],
     targets: [
         .target(name: "GraphQLRxSwift", dependencies: ["GraphQL", "Graphiti", "RxSwift"]),
